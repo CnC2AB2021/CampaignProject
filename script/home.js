ee40938497b67472dd8cd3e28c5778e16855e09c
Ziyang Qiu <ziyangqiu@outlook.com>
2021-12-14 21:36:14 -0500

    small changes

--- script/home.js
+++ script/home.js
@@ -1,3 +1,8 @@
 onready(() => {
   addStyle('home.css');
+
+  onload(() => {
+    let joinForm = document.getElementById('join__form');
+    joinForm.addEventListener('submit', () => setTimeout(() => joinForm.reset(), 200));
+  });
 });
